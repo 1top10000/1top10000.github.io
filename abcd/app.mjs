00505901641f85e2193f222af1c654884e0acb62
1top10000 <107670523+1top10000@users.noreply.github.com>
2026-09-12 16:22:28 +0900

    Delete abcd/app.mjs

--- abcd/app.mjs
+++
@@ -1,8 +0,0 @@
-import { createServer } from 'node:http';
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
-});
-server.listen(80, '0.0.0.0', () => {
-  console.log('0.0.0.0:80');
-});
